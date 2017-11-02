ab8f37c7bd9987fae1f346c6806961fe9ce7a78d
diksha0562 <30476228+diksha0562@users.noreply.github.com>
2017-11-02 08:12:52 +0530

    Add files via upload

--- components/App.js
+++ components/App.js
@@ -4,22 +4,33 @@ import React,{Component} from 'react';
 //create class app which extends component from react
 import {Form, FormControl,Button} from 'react-bootstrap';
 class App extends Component{
-    car_desc(){
-    //   const car = {props.car_desc};
-      console.log(this.props.value);
+    // car_desc(){
+    // //   const car = {props.car_desc};
+    //   console.log(this.props.value);
+    // }
+    constructor(){
+        super();
+        this.state={
+            selectValue : 'audi'
+        }
     }
+    // handleChange: function(e){
+    //     this.setState({selectValue : e.target.value});
+    // }
 render(){
+    var message='you selected ' + this.state.selectValue;
     return(
         <div>
         <h1 id="heading">Car Description</h1>
         <div className="dropdown">
-        <select id="myselect" value="asd" onChange={(e) => this.car_desc(e)}>
+        <select id="myselect"  onChange={function(event){this.setState({selectValue : event.target.value})}.bind(this)}>
             {/* <option selected="selected"> Select your car</option> */}
             <option value="audi"> Audi A5 </option>
             <option value= "bmw"> BMW 5 Series</option>
             <option value= "mercedes"> Mercedes-Benz GLC Class</option>
         </select>
         </div>
+        <p>{message}</p>
         </div>
     )
 }
